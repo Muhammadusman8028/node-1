4c169a6e346c2abfcc65ead65c9d55e5651abf64
usman <usmanmujahid6666@gmail.com>
2025-07-23 20:47:47 +0000

    aa

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World Abdullah. How are you doing. its newssssssssssss');
+  res.send('Hello, World Abdullah. How are you doing. its newsssssss');
 });
 
 app.listen(PORT, () => {
